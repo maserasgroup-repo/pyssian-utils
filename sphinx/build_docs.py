10e9602d9801cb8a486a4e0f3183b2779000d522
Raúl Pérez-Soto <rperezsoto.research@gmail.com>
2025-07-18 13:02:26 +0200

    Added basic tools for versioning of documentation

---
+++ sphinx/build_docs.py
@@ -0,0 +1,38 @@
+import os
+import subprocess
+import yaml 
+from pathlib import Path
+
+MAINBRANCH = 'master'
+BUILD_DIR = '_build'
+PAGES_DIR = 'docs'
+
+def build_doc(version, tag):
+    os.environ['current_version'] = version
+    # checkout to the tagged commit
+    subprocess.run(f'git checkout {tag}', shell=True)
+    # Recover the latest conf.py and latest versions.yaml
+    subprocess.run(f'git checkout {MAINBRANCH} -- conf.py', shell=True)
+    subprocess.run(f'git checkout {MAINBRANCH} -- versions.yaml', shell=True)
+    # build the docs
+    subprocess.run("make html", shell=True)
+
+def move_dir(src, dst):
+    Path(dst).mkdir(exist_ok=True)
+    if not src.endswith('/'):
+        src = f'{src}/'
+    subprocess.run(f'mv {src}* {dst}', shell=True)
+
+os.environ['build_all_docs'] = str(True)
+os.environ['pages_root'] = 'https://maserasgroup-repo.github.io/pyssian-utils' 
+
+build_doc('latest', MAINBRANCH)
+move_dir(f'./{BUILD_DIR}/html/', f'../{PAGES_DIR}/')
+
+with open('versions.yaml', 'r') as yaml_file:
+  docs = yaml.safe_load(yaml_file)
+
+for version, details in docs.items():
+    tag = details.get('tag', '')
+    build_doc(version, tag)
+    move_dir(f'./{BUILD_DIR}/html/', f'../{PAGES_DIR}/{version}/')
--- sphinx/source/pyssianutils.rst
+++ sphinx/source/pyssianutils.rst
@@ -1,35 +1,5 @@
-pyssianutils.functions
+pyssianutils.utils
 ======================
 
-.. automodule:: pyssianutils.functions
-
-General
--------
-
-.. autofunction:: pyssianutils.functions.write_2_file
-
-Script
-------
-
-.. autofunction:: pyssianutils.functions.potential_energy
-
-.. autofunction:: pyssianutils.functions.thermochemistry
-
-
-Console
--------
-
-.. autofunction:: pyssianutils.functions.print_thermo
-
-.. autofunction:: pyssianutils.functions.print_convergence
-
-
-Plot
-----
-
-.. autofunction:: pyssianutils.functions.PlotConvergence
-
-.. autofunction:: pyssianutils.functions.PlotInternalEnergy
-
-.. autofunction:: pyssianutils.functions.PlotThresholds
-
+.. automodule:: pyssianutils.utils
+    :members:
